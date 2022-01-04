021ac43721c4c744b4ff9f39769e046230898107
SivaDurgaRao <83358948+SivaDurgaRao@users.noreply.github.com>
2022-01-04 10:27:07 +0530

    Add files via upload

--- min.py
+++ min.py
@@ -1,14 +1,9 @@
-NumList = []
-Number = int(input("How many element in list, Please enter num : "))
-for i in range(1, Number+1):
-    value = int(input("Please enter the Value:"))
-    NumList.append(value)
- 
-smallest = NumList[0]
- 
-for j in range(1, Number):
-    if(smallest > NumList[j]):
-        smallest = NumList[j]
-
- 
-print("The Smallest Element in this List is : ", smallest)
+s={1,2,3,12,4,5,6}
+temp=0
+for i in s:
+    for j in s:
+        if i>j:
+            temp=j
+            j=i
+            i=temp
+print(i)
